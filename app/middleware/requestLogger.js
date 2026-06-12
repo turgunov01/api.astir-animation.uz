38a54e2c077ff6643abb7fd11e47db2f43ad681c
sardorceeksamurai@gmail.com <sardorceeksamurai@gmail.com>
2026-06-12 16:33:42 +0500

    requestlogger --port fix status

--- app/middleware/requestLogger.js
+++ app/middleware/requestLogger.js
@@ -12,18 +12,21 @@ export function requestLogger(request, response, next) {
   response.on("finish", () => {
     const contentLength = response.getHeader("content-length");
 
-    console.info(JSON.stringify({
-      // event: "http_request",
-      // requestId: request.id || null,
-      method: request.method,
-      url: request.originalUrl || request.url,
-      statusCode: response.statusCode,
-      // durationMs: Number(elapsedMs(startedAt).toFixed(2)),
-      ip: firstHeaderValue(request.get("x-forwarded-for")) || request.ip || request.socket?.remoteAddress || "",
-      userAgent: request.get("user-agent") || "",
-      // referer: request.get("referer") || request.get("referrer") || "",
-      // contentLength: contentLength === undefined ? null : Number(contentLength)
-    }));
+    if (request.statusCode >= 400) {
+      console.info(JSON.stringify({
+        // event: "http_request",
+        // requestId: request.id || null,
+        method: request.method,
+        url: request.originalUrl || request.url,
+        statusCode: response.statusCode,
+        // durationMs: Number(elapsedMs(startedAt).toFixed(2)),
+        ip: firstHeaderValue(request.get("x-forwarded-for")) || request.ip || request.socket?.remoteAddress || "",
+        userAgent: request.get("user-agent") || "",
+        // referer: request.get("referer") || request.get("referrer") || "",
+        // contentLength: contentLength === undefined ? null : Number(contentLength)
+      }));
+    };
+
   });
 
   next();
